e6d47a42fdfb9879dc9f8cda4363748039940e0f
saikiranRoyal <saikiranroyal111@gmail.com>
2022-12-08 09:47:57 +0530

    calculator component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalculatorComponent } from './calculator/calculator.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
@@ -7,8 +8,11 @@ import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 const routes: Routes = [
   {path:"login", component: LoginComponent},
   {path:"dashboard", component: DashboardComponent},
+  {path:"calculator", component: CalculatorComponent},
   {path:"", component: LoginComponent},
   {path:"**", component: PagenotfoundComponent},
+  
+
   
 ];
 
