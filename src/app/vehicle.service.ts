05f76a8207ca6c69cf79deb50729a0ccd78884c2
saikiranRoyal <saikiranroyal111@gmail.com>
2022-12-15 15:27:33 +0530

    getsortvehicle function column order type any -> type string

--- src/app/vehicle.service.ts
+++ src/app/vehicle.service.ts
@@ -21,7 +21,7 @@ export class VehicleService {
     return this._httpClient.get("https://6128991386a213001729f9df.mockapi.io/test/v1/jurisdiction?filter="+term);
 
   }
-  getsortvehicle(column:any, order:any):Observable<any>{
+  getsortvehicle(column:string, order:string):Observable<any>{
     return this._httpClient.get("https://6128991386a213001729f9df.mockapi.io/test/v1/jurisdiction?sortBy="+column+"&order="+order);
   }
 }
