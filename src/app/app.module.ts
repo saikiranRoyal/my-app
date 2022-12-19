c6e5bd4f638e81ed2846431174d42726239ea653
saikiranRoyal <saikiranroyal111@gmail.com>
2022-12-19 15:00:12 +0530

    login authentication using post method

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -43,6 +43,7 @@ import { CreateBankdetailsComponent } from './create-bankdetails/create-bankdeta
     BoredomComponent,
     CreateVehicleComponent,
     CreateBankdetailsComponent,
+  
     
   ],
   imports: [
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { LoginServiceService } from '../login-service.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  public loginForm:FormGroup=new FormGroup(
+    {
+      email:new FormControl(),
+      password:new FormControl(),
 
+    }
+    
+  )
+  constructor(private _loginService:LoginServiceService) {}
+  submit(){
+    console.log(this.loginForm.value)
+    this._loginService.loginAuthentication(this.loginForm.value).subscribe(
+      (data:any)=>{
+        alert("login successfull")
+      },
+      (err:any)=>{
+        alert("incorrect  password or username")
+      }
+    )
+  }
 }
